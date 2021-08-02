31a7aedb8249a7ba4a7b714d49bc3b6bb6d95da5
User Name <99ww77@gmail.com>
2021-08-02 16:14:29 +0300

    Initial commit

---
+++ stocks_products/logistic/serializers.py
@@ -0,0 +1,51 @@
+from rest_framework import serializers
+from .models import Product, Stock, StockProduct
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockProduct
+        fields = '__all__'
+        read_only_fields = ['stock']
+
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        #fields = '__all__'
+        fields = ["address", "positions"]
+
+
+
+
+    def create(self, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+        # создаем склад по его параметрам
+        stock = super().create(validated_data)
+
+        for position in positions:
+            StockProduct.objects.create(stock=stock, **position)
+
+        return stock
+
+
+    def update(self, instance, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+        # обновляем склад по его параметрам
+        stock = super().update(instance, validated_data)
+        for position in positions:
+            StockProduct.objects.update_or_create(stock=stock, **position)
+
+        return stock
